0f775cfb81ec2eb257c5626de29ab0ad2a1822e8
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-04-27 14:57:21 -0700

    Update project.m

--- project.m
+++ project.m
@@ -110,10 +110,10 @@ classdef groupProject < matlab.apps.AppBase
             timer_id = timer;
             timer_id.StartDelay = 1.0;
             timer_id.Period = 1.0;
-            % 周期性执行,fixedSpacing模式
+            % run it in fixedSpacing
             timer_id.ExecutionMode = 'fixedSpacing';
             timer_id.TimerFcn = @timer_handler;
-            %启动定时器
+            %start the timer
             start(timer_id);
             
         function timer_handler(~,~)
